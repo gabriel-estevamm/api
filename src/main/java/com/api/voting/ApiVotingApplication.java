d0a22aabd9a45f25b81df574654c075586cc536e
= <=>
2025-11-09 18:23:44 -0300

    init

---
+++ src/main/java/com/api/voting/ApiVotingApplication.java
@@ -0,0 +1,13 @@
+package com.api.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/voting/ApiVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
